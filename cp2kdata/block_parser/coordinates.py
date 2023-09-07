529081d306404b085fbcf7c8b61a493ceb27b419
weihong.xu <xuweihong.cn@gmail.com>
2023-09-07 10:00:35 +0800

    update coordinate matching rule

--- cp2kdata/block_parser/coordinates.py
+++ cp2kdata/block_parser/coordinates.py
@@ -3,7 +3,7 @@ import numpy as np
 
 INIT_ATOMIC_COORDINATES_RE = re.compile(
     r"""
-    \sMODULE\sQUICKSTEP:\s\sATOMIC\sCOORDINATES\sIN\sangstrom\s*\n
+    \sMODULE\sQUICKSTEP:\s+ATOMIC\sCOORDINATES\sIN\sangstrom\s*\n
     \n
     \s+Atom\s+Kind\s+Element\s+X\s+Y\s+Z\s+Z\(eff\)\s+Mass\s*\n
     (\n)?
@@ -20,7 +20,7 @@ INIT_ATOMIC_COORDINATES_RE = re.compile(
         \n
     )+
     """,
-    re.VERBOSE
+    re.VERBOSE | re.IGNORECASE,
 )
 
 
@@ -34,7 +34,7 @@ def parse_init_atomic_coordinates(output_file):
         init_atomic_coordinates.append([x, y, z])
     atom_kind_list = [int(kind) for kind in match.captures("kind")]
     chemical_symbols = match.captures("element")
-    
+
     if init_atomic_coordinates:
         return np.array(init_atomic_coordinates, dtype=float), np.array(atom_kind_list, dtype=int), chemical_symbols
     else:
