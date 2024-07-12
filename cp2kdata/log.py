5c56022f58782119fe5084cb638d5b1f45a333d1
robinzyb <38876805+robinzyb@users.noreply.github.com>
2024-07-12 17:15:44 +0200

    add integration by range for Cp2kCube class

--- cp2kdata/log.py
+++ cp2kdata/log.py
@@ -5,7 +5,10 @@ level_name = os.environ.get('CP2KDATA_LOG_LEVEL', 'INFO')
 level = logging._nameToLevel.get(level_name, logging.INFO)
 
 # format to include timestamp and module
-logging.basicConfig(format='CP2KDATA| %(levelname)-8s %(name)-40s: %(message)s', level=level)
+if level_name == 'DEBUG':
+    logging.basicConfig(format='CP2KDATA| %(asctime)s - %(levelname)-8s %(name)-40s: %(message)s', level=level)
+else:
+    logging.basicConfig(format='CP2KDATA| %(message)s', level=level)
 # suppress transitions logging
 # logging.getLogger('transitions.core').setLevel(logging.WARNING)
 
