ed0eed59b25dbc05b429749c15027a412e81776e
Yong-Bin Zhuang <38876805+robinzyb@users.noreply.github.com>
2025-12-18 21:53:57 +0900

    Refactor logging setup in log.py

--- cp2kdata/log.py
+++ cp2kdata/log.py
@@ -4,13 +4,25 @@ import os
 level_name = os.environ.get('CP2KDATA_LOG_LEVEL', 'INFO')
 level = logging._nameToLevel.get(level_name, logging.INFO)
 
-# format to include timestamp and module
-if level_name == 'DEBUG':
-    logging.basicConfig(format='CP2KDATA| %(asctime)s - %(levelname)-8s %(name)-40s: %(message)s', level=level)
-else:
-    logging.basicConfig(format='CP2KDATA| %(message)s', level=level)
-# suppress transitions logging
-# logging.getLogger('transitions.core').setLevel(logging.WARNING)
+logger = logging.getLogger(_LOGGER_NAME)
+logger.setLevel(level)
+logger.propagate = False  # do NOT touch root logger
+
+if not logger.handlers:
+    handler = logging.StreamHandler()
+
+    if level_name == "DEBUG":
+        formatter = logging.Formatter(
+            "CP2KDATA| %(asctime)s - %(levelname)-8s %(name)-40s: %(message)s"
+        )
+    else:
+        formatter = logging.Formatter("CP2KDATA| %(message)s")
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 
 def get_logger(name=None):
-    return logging.getLogger(name)
+    if name:
+        return logging.getLogger(f"{_LOGGER_NAME}.{name}")
+    return logger
